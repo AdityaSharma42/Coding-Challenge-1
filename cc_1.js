f6802aee3d9fd5ddac5bbea2a8fcc37d6adf06d3
AdityaSharma42 <adityasharma3@usf.edu>
2024-09-01 15:21:33 -0400

    Task 1- Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+let employeeName= "Steve Fleming";
+const employeeID= 479;
+var isActive= true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
