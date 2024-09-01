b72c29e15b7177517ce431bbac61cec602309cea
AdityaSharma42 <adityasharma3@usf.edu>
2024-09-01 15:42:22 -0400

    Task 2- Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -1,3 +1,4 @@
+//task 1
 let employeeName= "Steve Fleming";
 const employeeID= 479;
 var isActive= true;
@@ -5,3 +6,13 @@ var isActive= true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+//task 2
+
+let products = ["water bottle", "brita jug", "brita filter"]
+const productDetails={
+    name: Hydroflask
+    price: 39.99
+    inStock: true
+}
+console.log (products, productDetails);
